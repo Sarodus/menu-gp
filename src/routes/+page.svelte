e10422e21414d5e1d11d1966e9e6a2385249bf7c
jordi <jordiavellapol@gmail.com>
2023-01-21 21:42:32 +0100

    x

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-Welcome!
+<div class="container p-10">
+	Welcome!
+	<div>
+		<a href="/1607958b-00d4-4d62-bc47-ce3f41c30e48">Go to room</a>
+	</div>
+	<div>
+		<a href="/1607958b-00d4-4d62-bc47-ce3f41c30e48/results">See results</a>
+	</div>
+</div>
